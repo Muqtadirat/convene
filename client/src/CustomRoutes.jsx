1c1c98b05f86057b09bb318f2b0ad957bb855890
Muqtadirat <muqtadir.my@gmail.com>
2024-02-20 19:00:52 +0100

    feat: route setup

---
+++ client/src/CustomRoutes.jsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+
+
+const CustomRoutes = () => {
+    <Routes>
+        <Route path="/" element="" />
+    </Routes>
+}
+
+export default CustomRoutes
