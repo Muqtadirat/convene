b602366b549f8bb1b5a34345cac38cfca4c02bd9
Muqtadirat <muqtadir.my@gmail.com>
2024-02-21 15:37:51 +0100

    feat: login page route

--- client/src/CustomRoutes.jsx
+++ client/src/CustomRoutes.jsx
@@ -1,6 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import NavBar from "./components/navigation/NavBar";
+import Login from "./pages/Login";
+
+// const Layout = ({ children }) => {
+//   return(
+//   <>
+//       <NavBar />
+//       <Homepage />
+//   </>)
+// }
 
 const CustomRoutes = () => {
   return (
@@ -10,11 +19,12 @@ const CustomRoutes = () => {
         element={
           <>
             <NavBar />
-         
             <Homepage />
           </>
         }
       />
+
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
