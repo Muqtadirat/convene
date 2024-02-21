62c0f86fceaaedbf5c9f6a016d09a8d93fcb2025
Muqtadirat <muqtadir.my@gmail.com>
2024-02-21 15:37:27 +0100

    feat: updated button props

--- client/src/components/shared/buttons/index.jsx
+++ client/src/components/shared/buttons/index.jsx
@@ -1,11 +1,25 @@
-import styles from "./Button.module.css"
+import styles from "./Button.module.css";
+import clsx from "clsx";
 
-const Button = ({variant, children, width }) => {
-    return (
-        <button data-variant={variant} style={{width: width}} className={styles.btn}>
-            {children}
-        </button>
-    )
-}
+const Button = ({
+  variant,
+  children,
+  width,
+  className,
+  type,
+  onClickHandler,
+}) => {
+  return (
+    <button
+      data-variant={variant}
+      style={{ width: width }}
+      type={type}
+      onClick={onClickHandler}
+      className={clsx(styles.btn, className)}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
