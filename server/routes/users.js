eedb033fe716f60a5741dc8f7efeb05961d09598
Muqtadirat <muqtadir.my@gmail.com>
2024-02-22 13:30:38 +0100

    feat: endpoint update for POST

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,15 @@ router.get('/', function(req, res, next) {
   res.json({users})
 });
 
+// POST route to create a new user
+router.post('/', function(req, res, next) {
+  const { username, password, role } = req.body;
+  const newUser = { username, password, role };
+  users.push(newUser); 
+
+  res.status(201).json({ message: 'User created successfully', user: newUser });
+});
+
 const users = [
   {
     username: "Taddy",
