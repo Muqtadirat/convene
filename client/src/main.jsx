62b8eb4021169db7fc14d868f1c4b75102f6417a
Muqtadirat <muqtadir.my@gmail.com>
2024-02-23 23:04:00 +0100

    build: mantine UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from './App.jsx'
-import "./styles/globals.css"
-import "./styles/variables.css"
+import { MantineProvider } from "@mantine/core";
+import App from "./App.jsx";
+import "@mantine/core/styles.css";
+import "./styles/globals.css";
+import "./styles/variables.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <MantineProvider>
+      <Router>
+        <App />
+      </Router>
+    </MantineProvider>
   </React.StrictMode>
 );
