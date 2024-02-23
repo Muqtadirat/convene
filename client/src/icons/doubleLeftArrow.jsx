37fe1ecd75ad7e68b98b89ca4000119d4e988719
Muqtadirat <muqtadir.my@gmail.com>
2024-02-23 12:14:46 +0100

    chore: icons

---
+++ client/src/icons/doubleLeftArrow.jsx
@@ -0,0 +1,22 @@
+export default function DoubleLeftArrow() {
+    return (
+      <svg
+        height="21"
+        viewBox="0 0 21 21"
+        width="21"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fill="none"
+          fillRule="evenodd"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          transform="translate(5 6)"
+        >
+          <path d="m8.5 8.5-4-4 4-4" />
+          <path d="m4.5 8.5-4-4 4-4" />
+        </g>
+      </svg>
+    );
+}
--- client/src/icons/index.jsx
+++ client/src/icons/index.jsx
@@ -2,5 +2,11 @@ export { default as Logo } from "./logo";
 export { default as FullLogo } from "./fullLogo";
 export { default as Hamburger } from "./hamburger";
 export { default as BackArrow } from "./backArrow";
-export {default as EyeClosed} from "./eyeClosed"
-export {default as EyeOpen} from "./eyeOpen"
+export { default as EyeClosed } from "./eyeClosed";
+export { default as EyeOpen } from "./eyeOpen";
+export { default as DoubleRightArrow } from "./doubleRightArrow";
+export { default as DoubleLeftArrow } from "./doubleLeftArrow";
+export { default as Bell } from "./bell"
+export { default as Books } from "./books"
+export { default as Elements } from "./elements"
+export {default as Pie} from "./pie"
