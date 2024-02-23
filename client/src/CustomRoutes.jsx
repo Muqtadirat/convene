03899aabf6fdb5cc26f04042044eb0fe2d6b3608
Muqtadirat <muqtadir.my@gmail.com>
2024-02-23 13:17:54 +0100

    feat: dashboard route

--- client/src/CustomRoutes.jsx
+++ client/src/CustomRoutes.jsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import NavBar from "./components/navigation/NavBar";
+import SideBar from "./components/navigation/SideBar";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import AdminDashboard from "./pages/AdminDashboard";
 
-// const Layout = ({ children }) => {
-//   return(
-//   <>
-//       <NavBar />
-//       <Homepage />
-//   </>)
-// }
+const Layout = ({ children }) => {
+  return (
+    <>
+      <SideBar />
+      {children}
+    </>
+  );
+};
 
 const CustomRoutes = () => {
   return (
@@ -25,8 +28,17 @@ const CustomRoutes = () => {
         }
       />
 
+      <Route
+        path="/admindashboard"
+        element={
+          <Layout>
+            <AdminDashboard />
+          </Layout>
+        }
+      />
+
       <Route path="/login" element={<Login />} />
-      <Route path="/signUp" element={<SignUp/>} />
+      <Route path="/signUp" element={<SignUp />} />
     </Routes>
   );
 };
---
+++ client/src/pages/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+const AdminDashboard = () => {
+    return (
+        <div>
+            <header>Welcome to the admin dashboard</header>
+        </div>
+    )
+}
+
+export default AdminDashboard
