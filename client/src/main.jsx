9fc91db7b5e573ab13ac1b438b15b45ce754dc5f
Muqtadirat <muqtadir.my@gmail.com>
2024-03-01 23:22:17 +0100

    build: axios and hot toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,12 @@ import App from "./App.jsx";
 import "@mantine/core/styles.css";
 import "./styles/globals.css";
 import "./styles/variables.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider>
+      <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
       <Router>
         <App />
       </Router>
